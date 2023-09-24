d98b2a7e440c348601f0fcb05c8d67782334fa72
Daniel I <Daniel I@DanielIP>
2023-09-24 12:44:04 +0200

    a

--- src/ejercicio1/Ejercicio10.java
+++ src/ejercicio1/Ejercicio10.java
@@ -8,11 +8,11 @@ public class Ejercicio10 {
 	}
 	public static void main(String[] args) {
 		double precio, IVA;
-		IVA = calculoIVA(precio);
 		Scanner sc = new Scanner(System.in);
 		System.out.println("¿Cuanto dinero te has gastado?");
 		precio = sc.nextDouble();
-		System.out.println("Precio sin con IVA " +precio+IVA) ;
+		IVA = calculoIVA(precio);
+		System.out.println("Precio con IVA " +precio+IVA) ;
 		
 		
 		
